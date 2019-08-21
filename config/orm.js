c51d6b4759747230accaa5ab3bf0e6a48a9f2ce1
Pierce Keane <Pierce.Keane@gmail.com>
2019-08-21 12:54:20 -0400

    commiting work

--- config/orm.js
+++ config/orm.js
@@ -41,14 +41,57 @@ var orm = {
         })
     },
 
-    insertOne:function(table, cols, vals, callback) {
-        
+insertOne:function(table, cols, vals, callback) {
+    var queryString = "INSERT INTO " + table;
 
-    } 
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+
+},
+updateOne: function (table, objColVals, condition, callback) {
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            callback(result);
+        });
+},
+deleteOne: function (table, condition, callback) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+}); 
 
 
-
-
+}
 }
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,28 @@
 var orm = require("../config/orm.js");
+
+
+var burger = {
+    selectAll: function (Callback) {
+        orm.selectAll("burgers", function (res) {
+            Callback(res);
+        });
+    },
+    // The variables colums and values are arrays. Callback, and response
+    insertOne: function (cols, vals, Callback) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            Callback(res);
+        });
+    },
+    updateOne: function (objColVals, condition, Callback) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            Callback(res);
+        });
+    },
+    deleteOne: function (condition, Callback) {
+        orm.deleteOne("burgers", condition, function (res) {
+            Callback(res);
+        });
+    }
+};
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
